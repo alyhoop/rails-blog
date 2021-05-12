16c163c51086ddf9a98a62debc1c8ef53aa8f0bc
Alyssa Hooper <nesteralyssa@gmail.com>
2021-05-11 23:50:50 -0500

    rerenders articles and adds model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_060146) do
+ActiveRecord::Schema.define(version: 2021_05_12_041949) do
+
+  create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "delete_mes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
